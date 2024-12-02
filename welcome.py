61ee4150ba236272fe181e15b4a788dc75ae77ce
Jaron Bloom <j-bloom7@protonmail.com>
2024-12-02 02:34:12 -0500

    modify table header to expand the table width for better user experience and visuals

--- welcome.py
+++ welcome.py
@@ -1,21 +1,35 @@
 import datetime
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QTableWidget, QHeaderView
 from database import Database
 
 class MainWindow(QDialog):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi('welcomescreen.ui', self)
+
+        """
+        Create the database table with the name of the current year and month,
+        allowing the monthly overview to be displayed
+        """
         db = Database()
-        month = self.get_current_month_year()
+        month = self.get_current_year_month()
         db.create_table(month)
+
+        """
+        Setup the table object to modify the header
+        Stretch the header to fit the full width of the table
+        """
+        self.table = self.findChild(QTableWidget, "MonthlyTableOverview")
+
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.Stretch)
 
         
     """
-    Get current month and year for table creation formated as "YYYY_MM"
+    Get current year and month for table creation formated as "YYYY_MM"
     """
-    def get_current_month_year(self):
+    def get_current_year_month(self):
         today = datetime.datetime.now()
         current_month_year = today.strftime("%Y_%m")
         return current_month_year
