e1763a557b8d52772eee08afd8dc969c8629a93d
Jaron Bloom <j-bloom7@protonmail.com>
2024-12-03 02:02:58 -0500

    create add entry and load table functionality and repopulate table when new expense is created

--- expenses.py
+++ expenses.py
@@ -1,9 +1,58 @@
 import datetime
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QDialog, QTableWidget, QHeaderView
+from PyQt5.QtWidgets import QDialog, QTableWidget, QHeaderView, QMessageBox
 from database import Database
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+from PyQt5.QtCore import pyqtSignal, QDate
 
 class ExpenseDialog(QDialog):
+    
+    entry_added = pyqtSignal()
+
     def __init__(self):
         super(ExpenseDialog, self).__init__()
         loadUi('expenseDialog.ui', self)
+        self.expenseAddPushButton.clicked.connect(self.add_entry)
+                                                  
+    """
+    Add new entry to SQL database from user input
+    """
+    def add_entry(self):
+        date = self.expenseDateEdit.date().toString("yyyy-MM-dd")
+        store_name = self.expenseStoreEdit.text() 
+        category = self.expenseCategoryDropdown.currentText()
+        entry_type = "expense"
+        amount = self.expenseAmountSpinbox.text()
+        description = self.expenseDescriptionTextEdit.toPlainText()
+        month = self.get_current_year_month()
+        query = QSqlQuery()
+        query.prepare(f"""INSERT INTO '{month}' (date, store_name, category, entry_type, amount, description) 
+                            VALUES (:date, :store_name, :category, :entry_type, :amount, :description)
+                        """)
+        
+        query.bindValue(":date", date)
+        query.bindValue(":store_name", store_name)
+        query.bindValue(":category", category)
+        query.bindValue(":entry_type", entry_type)
+        query.bindValue(":amount", amount)
+        query.bindValue(":description", description)
+        
+        if not query.exec_():
+            error = query.lastError().text()
+            QMessageBox.warning(self, "Add Entry Failed", f"Failed to add entry: {error}")
+        else:
+            self.expenseDateEdit.setDate(QDate.currentDate())
+            self.expenseStoreEdit.clear() 
+            self.expenseCategoryDropdown.setCurrentIndex(0)
+            self.expenseAmountSpinbox.clear()
+            self.expenseDescriptionTextEdit.clear()
+            self.entry_added.emit()
+            self.close()
+
+    """
+    Get current year and month for table creation formated as "YYYY_MM"
+    """
+    def get_current_year_month(self):
+        today = datetime.datetime.now()
+        current_month_year = today.strftime("%Y_%m")
+        return current_month_year
--- welcome.py
+++ welcome.py
@@ -1,9 +1,10 @@
 import datetime
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QDialog, QTableWidget, QHeaderView
+from PyQt5.QtWidgets import QDialog, QTableWidget, QHeaderView, QTableWidgetItem
 from database import Database
 from expenses import ExpenseDialog
 from income import IncomeDialog
+from PyQt5.QtSql import QSqlQuery
 
 class MainWindow(QDialog):
     def __init__(self):
@@ -33,6 +34,7 @@ class MainWindow(QDialog):
         self.addExpenseBtn.clicked.connect(self.display_expense_dialog)
         self.addIncomeBtn.clicked.connect(self.display_income_dialog)
 
+        self.load_table()
         
     """
     Get current year and month for table creation formated as "YYYY_MM"
@@ -44,8 +46,42 @@ class MainWindow(QDialog):
     
     def display_expense_dialog(self):
         expense_dialog = ExpenseDialog()
+        expense_dialog.entry_added.connect(self.reload_table)
         expense_dialog.exec_()
 
     def display_income_dialog(self):
         income_dialog = IncomeDialog()
         income_dialog.exec_()
+
+
+    """
+    Populate the monthly overview table with the data from the database
+    """
+    def load_table(self):
+        self.table.setRowCount(0)
+        month = self.get_current_year_month()
+        query = QSqlQuery(f"SELECT * FROM '{month}' ORDER BY date DESC")
+        row = 0
+        while query.next():
+            date = query.value(1)
+            store_name = query.value(2)
+            category = query.value(3)
+            entry_type = query.value(4)
+            amount = query.value(5)
+            description = query.value(6)
+
+            self.table.insertRow(row)
+
+            self.table.setItem(row, 0, QTableWidgetItem(date))
+            self.table.setItem(row, 1, QTableWidgetItem(store_name))
+            self.table.setItem(row, 2, QTableWidgetItem(category))
+            self.table.setItem(row, 3, QTableWidgetItem(entry_type))
+            self.table.setItem(row, 4, QTableWidgetItem(str(amount)))
+            self.table.setItem(row, 5, QTableWidgetItem(description))
+
+            row += 1
+
+    def reload_table(self):
+        self.table.clearContents()
+        self.table.setRowCount(0)
+        self.load_table()
